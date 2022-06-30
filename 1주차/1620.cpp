afc06d33a1099eb0b5c57c0802acdf84bdfdd4df
Lim <ghktjq1119@naver.com>
2022-07-01 00:43:15 +0900

    Update 1620.cpp

--- 1주차/1620.cpp
+++ 1주차/1620.cpp
@@ -18,6 +18,8 @@ int main(){
 		string s;
 		cin >> s;
 		if(isdigit(s[0])==0){ // isdigit(char형) -> 문자일 경우 무조건 0이 나오고 숫자면 0이 아닌 다른값
+		if(atoi(s.c_str())==0) // c_str -> 문자열의 첫번째 주소값 , atoi -> 문자 -> 숫자 ( 문자가 나올경우 0이 나옴, 문자+숫자 일 경우
+		                       // 문자가 바로 나오면 바로 0을 반환하고 숫자가 먼저 나오면 숫자만 뽑혀서 나옴 
 			cout<< m1[s]<<'\n'; 
 		}else cout<< m2[stoi(s)]<<'\n';
 	}
