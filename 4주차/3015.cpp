2f44dbd22f93b72af57027693c28376259eab4b5
Lim <ghktjq1119@naver.com>
2022-07-26 19:42:55 +0900

    Create 3015.cpp

---
+++ 4주차/3015.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int n,ans;
+stack<pair<int,int>> stk;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n;
+
+	for(int i=0;i<n;i++) {
+		int x;
+		cin >> x;
+		int cnt = 1;
+
+		while (!stk.empty() && x >= stk.top().first) {
+			ans += stk.top().second;
+			if (x == stk.top().first) cnt = stk.top().second + 1; // 같은 수가 존재할 때 누적
+			else cnt = 1; 
+			stk.pop();
+		}
+		if (stk.size()) ans++; // 내림차순일때
+		stk.push({ x,cnt });	
+	}
+	cout << ans;
+}
