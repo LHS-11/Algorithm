cbc64d7300666e5878fe849f5581d58b1584e5b7
Lim <ghktjq1119@naver.com>
2022-07-29 22:16:17 +0900

    Create 13144.cpp

---
+++ 5주차/13144.cpp
@@ -0,0 +1,23 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int n;
+long long ans;
+int vis[100001];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		while (pq.size() && vis[x] == 1) {
+			vis[pq.top().second] = 0;
+			pq.pop();
+		}
+		pq.push({ i,x });
+		vis[x] = 1;
+
+		if (pq.size()) ans += pq.size();
+	}
+	cout << ans;
+}
