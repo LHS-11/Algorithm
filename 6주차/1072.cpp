697bb117521f2d1d160704293127904842c8dbbf
Lim <ghktjq1119@naver.com>
2022-08-15 23:28:15 +0900

    Create 1072.cpp

---
+++ 6주차/1072.cpp
@@ -0,0 +1,29 @@
+﻿#include<bits/stdc++.h>
+#define INF 1e13
+using namespace std;
+long long x, y, z;
+bool go(int k) {
+
+	if ((int)z < (int)((y + k)*100 / (x + k))) {
+		return true;
+	}
+	return false;
+}
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> x >> y;
+	long long l = 1, r = INF, m;
+	long long ans = INF;
+	z = y * 100 / x;
+	while (l <= r) {
+		m = (l + r) >> 1;
+		if (go(m)) {
+			ans = min(ans, m);
+			r = m - 1;
+		}
+		else l = m + 1;
+	}
+	if (ans == INF) cout << -1;
+	else cout << ans;
+	
+}
