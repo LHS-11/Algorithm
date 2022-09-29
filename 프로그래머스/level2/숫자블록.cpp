bb5f6812c5b92b0491457e93af5adc3842207598
LHS-11 <ghktjq1119@naver.com>
2022-09-30 01:53:14 +0900

    Create 숫자블록.cpp

---
+++ 프로그래머스/level2/숫자블록.cpp
@@ -0,0 +1,24 @@
+﻿#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+vector<int> solution(long long begin, long long end) {
+    vector<int> answer;
+
+    if (begin == 1) {
+        answer.push_back(0);
+        begin++;
+    }
+
+    for (ll st = begin; st <= end; st++) {
+        ll rst = 1;
+        for (ll i = 1; i * i <= st; i++) {
+            if (i != 1 && st % i == 0 && (st / i) <= 10000000) {
+                rst = max(rst, st / i);
+            }
+        }
+        answer.push_back(rst);
+    }
+
+    return answer;
+}
