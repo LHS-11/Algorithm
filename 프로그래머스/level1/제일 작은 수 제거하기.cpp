cca79208a821676ada1f6132ae0db6c1c567270d
LHS-11 <ghktjq1119@naver.com>
2022-09-30 01:53:11 +0900

    Create 제일 작은 수 제거하기.cpp

---
+++ 프로그래머스/level1/제일 작은 수 제거하기.cpp
@@ -0,0 +1,18 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    vector<int> ans(arr.size()); // copy 쓰기전에 벡터 크기 할당을 하고 사용해야함
+
+    copy(arr.begin(), arr.end(), ans.begin());
+
+    sort(ans.begin(), ans.end());
+
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == ans[0]) continue;
+        answer.push_back(arr[i]);
+    }
+    if (!answer.size()) answer.push_back(-1);
+    return answer;
+}
