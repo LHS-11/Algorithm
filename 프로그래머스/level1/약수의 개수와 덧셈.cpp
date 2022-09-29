56d0ffe5deaf53e8ab5449a72d22bb434043cc92
LHS-11 <ghktjq1119@naver.com>
2022-09-30 01:53:09 +0900

    Create 약수의 개수와 덧셈.cpp

---
+++ 프로그래머스/level1/약수의 개수와 덧셈.cpp
@@ -0,0 +1,27 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+bool go(int n) {
+    int cnt = 0;
+    if (n == 1) cnt += 1;
+    else cnt += 2;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            if (n == i * i) cnt += 1;
+            else cnt += 2;
+        }
+    }
+    if (cnt % 2 == 0) return true;
+    else return false;
+
+}
+int solution(int left, int right) {
+    int answer = 0;
+
+    for (int i = left; i <= right; i++) {
+        if (go(i)) answer += i;
+        else answer -= i;
+    }
+
+    return answer;
+}
