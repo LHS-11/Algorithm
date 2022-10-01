18858a30512993542b0ce22f69ac0a4a4d435232
LHS-11 <ghktjq1119@naver.com>
2022-10-02 01:11:47 +0900

    Create 행렬의 덧셈.cpp

---
+++ 프로그래머스/level1/행렬의 덧셈.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+
+    for (int i = 0; i < arr1.size(); i++) {
+        vector<int> ans;
+        for (int j = 0; j < arr1[i].size(); j++) {
+            ans.push_back(arr1[i][j] + arr2[i][j]);
+        }
+        answer.push_back(ans);
+    }
+
+    return answer;
+}
