0dd084ca051a2502dcd1cfb9a8c89065096a2068
LHS-11 <ghktjq1119@naver.com>
2022-10-02 01:11:45 +0900

    Create 이상한 문자 만들기 (허접 풀이).cpp

---
+++ 프로그래머스/level1/이상한 문자 만들기 (허접 풀이).cpp
@@ -0,0 +1,48 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+string answer = "";
+bool chk(char c) { // 대문자 체킹
+    if (c >= 'A' && c <= 'Z') return true;
+    else return false;
+}
+void change(string s1) {
+    for (int i = 0; i < s1.size(); i++) {
+        if (s1[i] != ' ') {
+            if (i % 2 == 0) {
+                if (!chk(s1[i])) answer += (char)s1[i] - 'a' + 'A';
+                else answer += s1[i];
+            }
+            else {
+                if (chk(s1[i])) answer += (char)s1[i] - 'A' + 'a';
+                else answer += s1[i];
+            }
+        }
+        else {
+            answer += ' ';
+        }
+    }
+}
+string solution(string s) {
+
+    string s2 = "";
+    string s3 = "";
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ' ') {
+            s3 += s[i];
+            change(s2);
+            s2 = "";
+        }
+        else {
+            s2 += s[i];
+            answer += s3;
+            s3 = "";
+        }
+    }
+    if (s2.size()) {
+        change(s2);
+    }
+    else {
+        answer += s3;
+    }
+    return answer;
+}
