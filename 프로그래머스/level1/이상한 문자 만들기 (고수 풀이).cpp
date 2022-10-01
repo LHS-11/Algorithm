5597a50fb9810f0208a5336fb43dc3d6d729e767
LHS-11 <ghktjq1119@naver.com>
2022-10-02 01:11:43 +0900

    Create 이상한 문자 만들기 (고수 풀이).cpp

---
+++ 프로그래머스/level1/이상한 문자 만들기 (고수 풀이).cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+
+    int idx = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ' ') {
+            answer += ' ';
+            idx = 0;
+        }
+        else {
+            idx & 1 ? answer += tolower(s[i]) : answer += toupper(s[i]);
+            idx++;
+        }
+    }
+
+    return answer;
+}
