e6427807bdb6bd05accb0572773e1f03e7cab88e
LHS-11 <ghktjq1119@naver.com>
2022-10-01 21:33:30 +0900

    Create 같은 숫자는 싫어.cpp

---
+++ 프로그래머스/level1/같은 숫자는 싫어.cpp
@@ -0,0 +1,22 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+stack<int> stk;
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    
+    for(auto c : arr){
+        if(stk.empty()){
+            stk.push(c);
+        }else{
+            if(stk.top()==c) continue;
+            stk.push(c);
+        }
+    }
+    while(stk.size()){
+        answer.push_back(stk.top());
+        stk.pop();
+    }
+    reverse(answer.begin(),answer.end());
+    return answer;
+}
