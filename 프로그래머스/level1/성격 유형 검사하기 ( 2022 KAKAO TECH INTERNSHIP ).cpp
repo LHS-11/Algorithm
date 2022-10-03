124e8bdcdbb6bc10bdd8a7cf698f67f6439a5a25
LHS-11 <ghktjq1119@naver.com>
2022-10-04 01:19:31 +0900

    Create 성격 유형 검사하기 ( 2022 KAKAO TECH INTERNSHIP ).cpp

---
+++ 프로그래머스/level1/성격 유형 검사하기 ( 2022 KAKAO TECH INTERNSHIP ).cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int score[] = { 3,2,1,0,1,2,3 };
+map<char, int> mp;
+string solution(vector<string> survey, vector<int> choices) {
+    string answer = "";
+
+    for (int i = 0; i < survey.size(); i++) {
+
+        choices[i] >= 4 ? mp[survey[i][1]] += score[choices[i] - 1] : mp[survey[i][0]] += score[choices[i] - 1];
+
+    }
+    mp['R'] >= mp['T'] ? answer += 'R' : answer += 'T';
+    mp['C'] >= mp['F'] ? answer += 'C' : answer += 'F';
+    mp['J'] >= mp['M'] ? answer += 'J' : answer += 'M';
+    mp['A'] >= mp['N'] ? answer += 'A' : answer += 'N';
+
+    return answer;
+}
