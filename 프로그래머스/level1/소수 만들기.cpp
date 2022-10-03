946fd6fdf95a786aadd1254b3ac242eca9695f86
LHS-11 <ghktjq1119@naver.com>
2022-10-04 01:18:47 +0900

    Create 소수 만들기.cpp

---
+++ 프로그래머스/level1/소수 만들기.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+bool check(int n){
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+int solution(vector<int> nums) {
+    int answer = 0;
+    
+    for(int i=0;i<nums.size();i++){
+        for(int j=i+1;j<nums.size();j++){
+            for(int k=j+1;k<nums.size();k++){
+                if(check(nums[i]+nums[j]+nums[k])) answer++;
+            }
+        }
+    }
+
+    return answer;
+}
