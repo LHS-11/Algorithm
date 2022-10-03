8a7c7add690256c0104f142eade4a9ef6c99163b
LHS-11 <ghktjq1119@naver.com>
2022-10-04 01:18:53 +0900

    Create 소수 찾기.cpp

---
+++ 프로그래머스/level1/소수 찾기.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+int go(int n) {
+    int cnt = 0;
+    for (int i = 2; i <= n; i++) { // 2~n번까지 확인
+        int flag = 0;
+        for (int j = 2; j * j <= i; j++) { // 약수인지 확인
+            if (i % j == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if (!flag) cnt++;
+    }
+
+    return cnt;
+}
+
+int solution(int n) {
+    return go(n);
+}
