177ded77b44c335e76912c75a8c6d00e183f22be
LHS-11 <ghktjq1119@naver.com>
2022-10-03 16:35:29 +0900

    Create 로또의 최고 순위와 최저 순위.cpp

---
+++ 프로그래머스/level1/로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+int ranking[] = { 6,6,5,4,3,2,1 };
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+    vector<int> answer;
+    int same = 0;
+    int cnt = 0;
+    for (int i = 0; i < lottos.size(); i++) {
+        if (!lottos[i]) cnt++;
+        for (int j = 0; j < win_nums.size(); j++) {
+            if (lottos[i] == win_nums[j]) same++;
+        }
+    }
+
+    answer.push_back(ranking[same + cnt]);
+    answer.push_back(ranking[same]);
+
+    return answer;
+}
