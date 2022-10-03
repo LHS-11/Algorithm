674df988729bc8e08e99d8802c6cbf33c30da895
LHS-11 <ghktjq1119@naver.com>
2022-10-03 16:35:33 +0900

    Create 문자열 내 마음대로 정렬하기.cpp

---
+++ 프로그래머스/level1/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,14 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int x;
+bool cmp(string a, string b) {
+    // if(a[x]==b[x]) return a<b;
+    // return a[x]<b[x];
+
+    return  a[x] == b[x] ? a < b : a[x] < b[x];
+}
+vector<string> solution(vector<string> strings, int n) {
+    x = n;
+    sort(strings.begin(), strings.end(), cmp);
+    return strings;
+}
