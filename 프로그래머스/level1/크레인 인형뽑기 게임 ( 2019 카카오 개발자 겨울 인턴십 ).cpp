02d7dd164e26c5c0595fd7f1100412fa42fe4f75
LHS-11 <ghktjq1119@naver.com>
2022-10-05 15:37:16 +0900

    Create 크레인 인형뽑기 게임 ( 2019 카카오 개발자 겨울 인턴십 ).cpp

---
+++ 프로그래머스/level1/크레인 인형뽑기 게임 ( 2019 카카오 개발자 겨울 인턴십 ).cpp
@@ -0,0 +1,27 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+
+    int sz = board.size();
+
+    stack<int> stk;
+
+    for (auto c : moves) {
+        for (int i = 0; i < sz; i++) {
+            if (board[i][c - 1]) {
+                if (stk.size() && stk.top() == board[i][c - 1]) {
+                    answer += 2;
+                    stk.pop();
+                }
+                else stk.push(board[i][c - 1]);
+                board[i][c - 1] = 0;
+                break;
+            }
+        }
+
+    }
+
+    return answer;
+}
