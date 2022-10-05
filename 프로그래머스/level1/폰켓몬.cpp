e46d7ca78f270a2fcb126b535dabcb8e6492cf9f
LHS-11 <ghktjq1119@naver.com>
2022-10-05 15:37:10 +0900

    Create 폰켓몬.cpp

---
+++ 프로그래머스/level1/폰켓몬.cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int solution(vector<int> nums)
+{
+    //     int answer = 0;
+    //     int sz=nums.size()/2;
+
+    //     sort(nums.begin(),nums.end());
+    //     nums.erase(unique(nums.begin(),nums.end()),nums.end());
+
+    //     nums.size()>sz ? answer=sz : answer=nums.size();
+        // return answer;
+
+    set<int> s;
+
+    for (auto c : nums) s.insert(c);
+
+    return min(s.size(), nums.size() / 2);
+
+
+}
