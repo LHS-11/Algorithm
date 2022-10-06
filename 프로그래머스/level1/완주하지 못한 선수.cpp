8e4d2828f83aaf4680f2354f067819a56929c9e8
LHS-11 <ghktjq1119@naver.com>
2022-10-06 18:56:36 +0900

    Create 완주하지 못한 선수.cpp

---
+++ 프로그래머스/level1/완주하지 못한 선수.cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+map<string, int> mp;
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+
+    for (auto c : completion) {
+        mp[c]++;
+    }
+
+    for (auto c : participant) {
+        if (!mp[c]) {
+            answer = c;
+            break;
+        }
+        else {
+            mp[c]--;
+        }
+    }
+    return answer;
+}
