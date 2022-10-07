2e02a526f9aaacbeda310985fd0af95dde66f477
LHS-11 <ghktjq1119@naver.com>
2022-10-07 12:52:10 +0900

    Create 숫자의 표현.cpp

---
+++ 프로그래머스/level2/숫자의 표현.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    // for(int i=1;i<=n;i++){
+    //     int sum=0;
+    //     for(int j=i;j<=n;j++){
+    //         sum+=j;
+    //         if(sum==n) answer++;
+    //         else if(sum>n) break;
+    //     }
+    // }
+
+    int sum1 = 0;
+    for (int i = 0; i < n; i++) {
+        sum1 += i;
+        int k = n - sum1;
+        int div = i + 1;
+
+        if (k >= div && k % div == 0) answer++;
+
+    }
+
+    return answer;
+}
