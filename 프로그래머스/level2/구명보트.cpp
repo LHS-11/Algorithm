0be92a8bd6189c0dee8e181174205ab210532707
LHS-11 <ghktjq1119@naver.com>
2022-10-08 18:44:58 +0900

    Create 구명보트.cpp

---
+++ 프로그래머스/level2/구명보트.cpp
@@ -0,0 +1,43 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int vis[50004];
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+
+    sort(people.begin(), people.end());
+
+    int en = people.size() - 1;
+
+
+    for (int st = 0; st < people.size(); st++) {
+
+        if (st > en) {
+            for (int i = 0; i <= en; i++) if (!vis[i]) answer++;
+            break;
+        }
+
+        while (st<en && people[st] + people[en]>limit) {
+            vis[en]++;
+            answer++;
+            en--;
+        }
+        if (people[st] + people[en] <= limit) {
+            vis[st]++;
+            vis[en]++;
+            en--;
+            answer++;
+        }
+    }
+    // sort(people.begin(), people.end());
+    // reverse(people.begin(), people.end());
+    // int N = (int)people.size();
+    // int res = 0;
+    // for (int i = 0, j = N - 1; i <= j; i++) {
+    //     if (people[i] + people[j] <= limit) {
+    //         j--;
+    //     }
+    //     res++;
+    // }
+    // return res;
+    return answer;
+}
