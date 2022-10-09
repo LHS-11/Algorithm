76ef3198ddb33058cf5a5bf8d696ad788f2e0d58
LHS-11 <ghktjq1119@naver.com>
2022-10-09 13:49:40 +0900

    Create 올바른 괄호.cpp

---
+++ 프로그래머스/level2/올바른 괄호.cpp
@@ -0,0 +1,23 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+
+    stack<char>stk;
+
+    for (auto c : s) {
+
+        if (c == ')')
+            if (stk.empty()) {
+                answer = false;
+                break;
+            }
+            else stk.pop();
+        else stk.push('(');
+    }
+    if (stk.size()) answer = false;
+
+    return answer;
+}
