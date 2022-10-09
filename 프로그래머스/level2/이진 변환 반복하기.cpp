eed07d4a750b18d132da6f63a695eb440d84a0e4
LHS-11 <ghktjq1119@naver.com>
2022-10-09 13:49:42 +0900

    Create 이진 변환 반복하기.cpp

---
+++ 프로그래머스/level2/이진 변환 반복하기.cpp
@@ -0,0 +1,27 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(string s) {
+
+    vector<int> answer;
+
+    int zeroCnt = 0, cnt = 0;
+    while (s != "1") {
+        int sz = 0;
+        for (auto c : s) {
+            if (c != '0') sz++;
+            else zeroCnt++;
+        }
+
+        s = "";
+        while (sz) {
+            s = s + to_string(sz % 2);
+            sz /= 2;
+        }
+        cnt++;
+    }
+    answer.push_back(cnt);
+    answer.push_back(zeroCnt);
+
+    return answer;
+}
