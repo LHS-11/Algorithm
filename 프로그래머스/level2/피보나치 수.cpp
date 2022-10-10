47b69206dbb0746e0e3eb2bfc1bd178de6f6b9a6
LHS-11 <ghktjq1119@naver.com>
2022-10-10 12:50:35 +0900

    Create 피보나치 수.cpp

---
+++ 프로그래머스/level2/피보나치 수.cpp
@@ -0,0 +1,25 @@
+﻿// 이 문제는 메모이제이션을 해서 풀되 Top-bottom 방식으로는 풀 수 없음 
+#include<bits/stdc++.h>
+using namespace std;
+int dp[100004];
+int go(int k) {
+    if (k == 0) return 0;
+    else if (k == 1) return 1;
+
+    int& ans = dp[k];
+    if (ans != -1) return ans;
+
+    return ans = (go(k - 1) + go(k - 2)) % 1234567;
+}
+int solution(int n) {
+    int answer = 0;
+    memset(dp, -1, 100004);
+    //answer=go(n)%1234567;
+
+    dp[0] = 0, dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+    answer = dp[n];
+    return answer;
+}
