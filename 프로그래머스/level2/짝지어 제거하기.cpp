fb4023c12f252114c4e2007b48c4c8721791b3de
LHS-11 <ghktjq1119@naver.com>
2022-10-10 12:50:30 +0900

    Create 짝지어 제거하기.cpp

---
+++ 프로그래머스/level2/짝지어 제거하기.cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int solution(string s)
+{
+    int answer = -1;
+
+    stack<int> stk;
+
+    for (auto c : s) {
+        if (stk.empty()) {
+            stk.push(c);
+        }
+        else {
+            if (stk.top() == c) {
+                stk.pop();
+            }
+            else stk.push(c);
+        }
+    }
+
+    if (stk.size()) answer = 0;
+    else answer = 1;
+
+    return answer;
+}
