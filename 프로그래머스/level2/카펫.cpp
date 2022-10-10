67d34d6d65f3f633d3f6ef6183254e6fd04bfb56
LHS-11 <ghktjq1119@naver.com>
2022-10-10 12:50:32 +0900

    Create 카펫.cpp

---
+++ 프로그래머스/level2/카펫.cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+
+    double x, y; // 가로, 세로
+
+    double bro = brown, yel = yellow;
+
+    for (int i = 3; i <= 5000; i++) { // 가로 
+        if ((bro + 4) / 2 - i == (yel / (i - 2) + 2)) {
+            y = (bro + 4) / 2 - i;
+            x = i;
+            break;
+        }
+    }
+    answer.push_back(max(x, y));
+    answer.push_back(min(x, y));
+
+
+
+
+    return answer;
+}
