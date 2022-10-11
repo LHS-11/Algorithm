3a9c6485e7cb80f0a72072d57ec83480ec23147d
LHS-11 <ghktjq1119@naver.com>
2022-10-11 11:02:17 +0900

    Create H-Index.cpp

---
+++ 프로그래머스/level2/H-Index.cpp
@@ -0,0 +1,27 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+bool go(int k, vector<int> v) {
+    int cnt = 0;
+    for (auto c : v) {
+        if (c >= k) cnt++;
+    }
+    if (cnt < k) return true;
+    else return false;
+}
+int solution(vector<int> citations) {
+    int answer = 0;
+
+    int l = 0, r = 10004;
+    while (l <= r) {
+        int m = (l + r) >> 1;
+        if (go(m, citations)) {
+            r = m - 1;
+        }
+        else {
+            l = m + 1;
+            answer = max(answer, m);
+        }
+    }
+
+    return answer;
+}
