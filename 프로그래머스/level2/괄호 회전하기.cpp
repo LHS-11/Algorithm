c17c57d38cb2ff932415cf98b70e5faed17aafdf
LHS-11 <ghktjq1119@naver.com>
2022-10-11 11:02:21 +0900

    Create 괄호 회전하기.cpp

---
+++ 프로그래머스/level2/괄호 회전하기.cpp
@@ -0,0 +1,31 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int solution(string s) {
+	int answer = 0;
+
+	int sz = s.size();
+
+	for (int i = 0; i < sz; i++) {
+		string s1 = s;
+		rotate(s1.begin(), s1.begin() + i, s1.end());
+
+		stack<char> stk;
+
+		for (auto c : s1) {
+
+			if (!stk.empty()) {
+				if ((stk.top() == '(' && c == ')')
+					|| (stk.top() == '{' && c == '}')
+					|| (stk.top() == '[' && c == ']')) {
+					stk.pop();
+				}
+				else stk.push(c);
+
+			}
+			else stk.push(c);
+
+		}
+		if (stk.empty()) answer++;
+	}
+	return answer;
+}
