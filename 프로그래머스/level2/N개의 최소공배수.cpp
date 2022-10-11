4a3ad3b191aeaab3faec72ca85c8a77e9d6ecd00
LHS-11 <ghktjq1119@naver.com>
2022-10-11 11:02:19 +0900

    Create N개의 최소공배수.cpp

---
+++ 프로그래머스/level2/N개의 최소공배수.cpp
@@ -0,0 +1,22 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b) {
+    if (a == 0) return b;
+    return gcd(b % a, a);
+}
+int lcm(int a, int b) {
+    return a * b / gcd(a, b);
+}
+int solution(vector<int> arr) {
+    int answer = 0;
+
+    answer = arr[0];
+
+
+    for (int i = 1; i < arr.size(); i++) {
+        answer = lcm(arr[i], answer);
+    }
+
+
+    return answer;
+}
