a54faec1418d87f3e271c88a829b4c04bbe3e381
LHS-11 <ghktjq1119@naver.com>
2022-10-12 13:38:37 +0900

    Create n^2 배열 자르기.cpp

---
+++ 프로그래머스/level2/n^2 배열 자르기.cpp
@@ -0,0 +1,15 @@
+﻿#include <string>
+#include <vector>
+typedef long long ll;
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+
+    for (ll i = left; i <= right; i++) {
+        answer.push_back(max(i / n, i % n) + 1);
+    }
+
+
+    return answer;
+}
