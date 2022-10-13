c9253efba05b394a704d7647590cda649f1fe98f
LHS-11 <ghktjq1119@naver.com>
2022-10-13 14:58:58 +0900

    Create 위장.cpp

---
+++ 프로그래머스/level2/위장.cpp
@@ -0,0 +1,20 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+map<string, int> mp;
+map<string, int> vis;
+int solution(vector<vector<string>> clothes) {
+
+    for (int i = 0; i < clothes.size(); i++) {
+        mp[clothes[i][1]]++;
+    }
+
+    int sum = 1;
+    for (auto c : clothes) {
+        if (!vis[c[1]]) {
+            vis[c[1]] = 1;
+            sum *= (mp[c[1]] + 1);
+        }
+    }
+
+    return sum - 1;
+}
