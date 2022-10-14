e498edff4390778537dfe1f247f94355d3c218c2
LHS-11 <ghktjq1119@naver.com>
2022-10-14 15:00:40 +0900

    Create 점프와 순간 이동.cpp

---
+++ 프로그래머스/level2/점프와 순간 이동.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+
+    // 이 문제는 수학 문제 => 최대로 순간 이동을 하는 순간의 건전지 수
+    while (n) {
+        if (n % 2 != 0) ans++;
+        n /= 2;
+    }
+
+    return ans;
+}
