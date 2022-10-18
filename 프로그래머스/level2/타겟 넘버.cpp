1fa2ad98f0eed6482114ecdd884455ed0999967e
LHS-11 <ghktjq1119@naver.com>
2022-10-18 14:12:44 +0900

    Create 타겟 넘버.cpp

---
+++ 프로그래머스/level2/타겟 넘버.cpp
@@ -0,0 +1,30 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int cnt;
+vector<int> v;
+void go(int idx, int a, int k, int sz) {
+
+    if (idx == sz) {
+        if (a == k) cnt++;
+        return;
+    }
+
+    go(idx + 1, a + v[idx], k, sz);
+    go(idx + 1, a - v[idx], k, sz);
+
+}
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+
+    v.resize(numbers.size());
+
+    copy(numbers.begin(), numbers.end(), v.begin());
+
+
+    go(0, 0, target, numbers.size());
+
+    answer = cnt;
+
+
+    return answer;
+}
