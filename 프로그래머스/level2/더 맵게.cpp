c3650f7910c9cc460e083be690e68991bc6d02e6
LHS-11 <ghktjq1119@naver.com>
2022-10-19 18:56:29 +0900

    Create 더 맵게.cpp

---
+++ 프로그래머스/level2/더 맵게.cpp
@@ -0,0 +1,26 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+priority_queue<int, vector<int>, greater<int>> q;
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+
+    for (auto c : scoville) q.push(c);
+
+    // priority_queue<int,vector<int>,greater<int>> q(scoville.begin(),scoville.end());
+
+
+    while (q.size() && q.top() < K) {
+        int cur = q.top();
+        q.pop();
+        if (q.empty()) {
+            answer = -1;
+            break;
+        }
+        int nxt = q.top();
+        q.pop();
+
+        q.push(cur + nxt * 2);
+        answer++;
+    }
+    return answer;
+}
