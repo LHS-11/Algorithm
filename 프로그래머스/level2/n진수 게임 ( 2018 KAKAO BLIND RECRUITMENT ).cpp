d3e6cf935dbda3bc1495caeeab0889c37d6630f0
LHS-11 <ghktjq1119@naver.com>
2022-10-20 16:17:56 +0900

    Create n진수 게임 ( 2018 KAKAO BLIND RECRUITMENT ).cpp

---
+++ 프로그래머스/level2/n진수 게임 ( 2018 KAKAO BLIND RECRUITMENT ).cpp
@@ -0,0 +1,52 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+vector<char> v;
+void change(int n, int t, int m) {
+
+    v.push_back('0');
+    for (int i = 1; i <= t * m; i++) {
+        int j = i;
+        deque<char> v1;
+        while (j) {
+            if (n >= 11) {
+                switch (j % n) {
+                case 10: v1.push_front('A');
+                    break;
+                case 11: v1.push_front('B');
+                    break;
+                case 12: v1.push_front('C');
+                    break;
+                case 13: v1.push_front('D');
+                    break;
+                case 14: v1.push_front('E');
+                    break;
+                case 15: v1.push_front('F');
+                    break;
+                default: v1.push_front((j % n) + '0');
+                    break;
+                }
+            }
+            else {
+                v1.push_front(((j % n) + '0'));
+            }
+            j /= n;
+
+        }
+        for (auto c : v1) v.push_back(c);
+    }
+
+}
+string solution(int n, int t, int m, int p) {
+    string answer = "";
+
+    change(n, t, m);
+
+    int k = 0;
+    while (answer.size() != t) {
+        answer += v[p + m * k - 1];
+        k++;
+    }
+
+    return answer;
+}
+
