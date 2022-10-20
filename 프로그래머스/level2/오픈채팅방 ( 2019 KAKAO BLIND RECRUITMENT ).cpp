e2ff2be811bc4b4a1f413be589b320f48429bcd6
LHS-11 <ghktjq1119@naver.com>
2022-10-20 16:18:00 +0900

    Create 오픈채팅방 ( 2019 KAKAO BLIND RECRUITMENT ).cpp

---
+++ 프로그래머스/level2/오픈채팅방 ( 2019 KAKAO BLIND RECRUITMENT ).cpp
@@ -0,0 +1,37 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+map<string, string> mp;
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+
+    for (auto s : record) {
+
+        stringstream stream;
+        stream.str(s);
+        string ss;
+        vector<string> v;
+        while (stream >> ss) {
+            v.push_back(ss);
+        }
+
+        if (v[0][0] == 'E' || v[0][0] == 'C') mp[v[1]] = v[2];
+    }
+
+    for (auto s : record) {
+
+        stringstream stream;
+        stream.str(s);
+        string ss;
+        vector<string> v;
+        while (stream >> ss) {
+            v.push_back(ss);
+        }
+
+        string name = mp[v[1]];
+        if (v[0][0] == 'E') answer.push_back(name + "님이 들어왔습니다.");
+        else if (v[0][0] == 'L') answer.push_back(name + "님이 나갔습니다.");
+    }
+
+
+    return answer;
+}
