7d0cb61c249d2bd1aca7204ffbde0d48c12f94cf
LHS-11 <ghktjq1119@naver.com>
2022-10-22 00:57:04 +0900

    Create 땅따먹기.cpp

---
+++ 프로그래머스/level2/땅따먹기.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int dp[100004][4], ans;
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+
+
+    for (int i = 0; i < 4; i++) dp[0][i] = land[0][i];
+
+    for (int i = 1; i < land.size(); i++) {
+
+        dp[i][0] = max({ dp[i - 1][1],dp[i - 1][2],dp[i - 1][3] }) + land[i][0];
+        dp[i][1] = max({ dp[i - 1][0],dp[i - 1][2],dp[i - 1][3] }) + land[i][1];
+        dp[i][2] = max({ dp[i - 1][1],dp[i - 1][0],dp[i - 1][3] }) + land[i][2];
+        dp[i][3] = max({ dp[i - 1][1],dp[i - 1][2],dp[i - 1][0] }) + land[i][3];
+
+    }
+    int sz = land.size() - 1;
+    answer = max({ dp[sz][0],dp[sz][1],dp[sz][2],dp[sz][3] });
+
+    return answer;
+}
