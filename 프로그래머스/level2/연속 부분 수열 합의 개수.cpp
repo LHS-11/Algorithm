a535cb861a840acddc62bdef7ce799d75d2a9109
LHS-11 <ghktjq1119@naver.com>
2022-10-23 21:39:54 +0900

    Create 연속 부분 수열 합의 개수.cpp

---
+++ 프로그래머스/level2/연속 부분 수열 합의 개수.cpp
@@ -0,0 +1,30 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int sumfix[3004];
+int vis[1000004];
+int solution(vector<int> elements) {
+    int answer = 0;
+
+    int sz = elements.size();
+
+    for (int i = 0; i < sz - 1; i++) elements.push_back(elements[i]);
+
+
+    for (int i = 0; i < elements.size(); i++) {
+        sumfix[i + 1] = sumfix[i] + elements[i];
+    }
+
+    for (int i = 1; i <= sz; i++) {
+        for (int j = 0; j < sz; j++) {
+            int sum = sumfix[i + j] - sumfix[j];
+            if (!vis[sum] && sum > 0) {
+                answer++;
+                vis[sum] = 1;
+            }
+        }
+    }
+
+
+
+    return answer;
+}
