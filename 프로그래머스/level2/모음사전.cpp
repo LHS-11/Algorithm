520cc5f2200b31d2c90688d91f1af6998d4c768b
LHS-11 <ghktjq1119@naver.com>
2022-10-24 15:13:30 +0900

    Create 모음사전.cpp

---
+++ 프로그래머스/level2/모음사전.cpp
@@ -0,0 +1,28 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+string s[5] = { "A","E","I","O","U" };
+vector<string> ans;
+void go(int sz, string ss) {
+    if (sz == ss.size()) {
+        ans.push_back(ss);
+        return;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        go(sz, ss + s[i]);
+    }
+    return;
+}
+int solution(string word) {
+    int answer = 0;
+
+    for (int i = 1; i <= 5; i++) {
+        go(i, "");
+    }
+
+    sort(ans.begin(), ans.end());
+
+    answer = find(ans.begin(), ans.end(), word) - ans.begin() + 1;
+
+    return answer;
+}
