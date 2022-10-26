c68e8d93e9b3519f4feff09261b1703905c5b055
LHS-11 <ghktjq1119@naver.com>
2022-10-26 16:19:48 +0900

    Create 2개 이하로 다른 비트.cpp

---
+++ 프로그래머스/level2/2개 이하로 다른 비트.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <vector>
+typedef long long ll;
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer;
+
+    for (auto n : numbers) {
+        ll bit = 1;
+        while ((n & bit) != 0) bit <<= 1;
+        answer.push_back(n + bit - (bit >> 1));
+    }
+
+
+    return answer;
+}
