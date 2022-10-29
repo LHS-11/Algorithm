c6bf635e6dbc176f1ec349e90ad1666a247e28d8
LHS-11 <ghktjq1119@naver.com>
2022-10-29 17:44:56 +0900

    Create 가장 큰 수.cpp

---
+++ 프로그래머스/level2/가장 큰 수.cpp
@@ -0,0 +1,24 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+bool compare(string s1, string s2) {
+    return s1 + s2 > s2 + s1;
+}
+string solution(vector<int> numbers) {
+    string answer = "";
+
+    vector<string> str;
+
+    for (auto n : numbers) {
+        str.push_back(to_string(n));
+    }
+
+    sort(str.begin(), str.end(), compare);
+
+    if (str[0] == "0") return "0";
+
+    for (auto s : str) {
+        answer += s;
+    }
+
+    return answer;
+}
