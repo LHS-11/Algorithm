be91caaa68bd72c6fd55da3b687b1948c72d6b4a
LHS-11 <ghktjq1119@naver.com>
2022-10-30 17:30:54 +0900

    Create 소수 찾기.cpp

---
+++ 프로그래머스/level2/소수 찾기.cpp
@@ -0,0 +1,53 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+vector<char> v;
+map<int, int> mp;
+int ans, vis[8];
+char flag;
+bool check(int k) {
+
+    if (k == 1 || k == 0) return false;
+
+    for (int i = 2; i * i <= k; i++) {
+        if (k % i == 0) return false;
+    }
+    return true;
+}
+void go(int sz, string str) {
+
+    if (v.size() == sz) {
+
+        string s = "";
+        for (auto c : v) s += c;
+
+        if (!mp[stoi(s)] && check(stoi(s))) {
+            ans++;
+            mp[stoi(s)]++;
+        }
+
+        return;
+    }
+
+    flag = '*';
+
+    for (int i = 0; i < str.size(); i++) {
+        if (vis[i] || flag == str[i]) continue;
+        vis[i] = 1;
+        v.push_back(str[i]);
+        go(sz, str);
+        flag = str[i];
+        v.pop_back();
+        vis[i] = 0;
+    }
+
+
+}
+int solution(string numbers) {
+
+    for (int i = 1; i <= numbers.size(); i++) {
+        go(i, numbers);
+        memset(vis, 0, sizeof(vis));
+        v.clear();
+    }
+    return ans;
+}
