5a35375654aa9fa94abbc03ec19d1a2e6664a6af
LHS-11 <ghktjq1119@naver.com>
2022-10-31 13:57:33 +0900

    Create 쿼드압축 후 개수 세기.cpp

---
+++ 프로그래머스/level2/쿼드압축 후 개수 세기.cpp
@@ -0,0 +1,32 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int oneCnt, zeroCnt;
+void go(int x, int y, int k, vector<vector<int>>& arr) {
+
+
+    int t = arr[y][x];
+    for (int i = y; i < y + k; i++) {
+        for (int j = x; j < x + k; j++) {
+            if (arr[i][j] != t) {
+                go(x, y, k / 2, arr);
+                go(x + k / 2, y, k / 2, arr);
+                go(x, y + k / 2, k / 2, arr);
+                go(x + k / 2, y + k / 2, k / 2, arr);
+                return;
+            }
+        }
+    }
+
+    if (t) oneCnt++;
+    else zeroCnt++;
+    return;
+
+}
+vector<int> solution(vector<vector<int>> arr) {
+
+    go(0, 0, arr.size(), arr);
+
+    vector<int> answer = { zeroCnt,oneCnt };
+
+    return answer;
+}
