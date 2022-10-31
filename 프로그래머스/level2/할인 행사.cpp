2ad424656cbb6f7f59bece76e78bbe2c16a42006
LHS-11 <ghktjq1119@naver.com>
2022-10-31 15:05:51 +0900

    Create 할인 행사.cpp

---
+++ 프로그래머스/level2/할인 행사.cpp
@@ -0,0 +1,28 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+map<string, int> mp;
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
+    int answer = 0;
+
+    for (int i = 0; i < want.size(); i++) {
+        mp[want[i]] = number[i];
+    }
+
+    for (int i = 0; i <= discount.size() - 10; i++) {
+        map<string, int>mp1 = mp;
+        for (int j = i; j < i + 10; j++) {
+            if (mp1[discount[j]]) {
+                mp1[discount[j]]--;
+            }
+        }
+
+        int flag = 0;
+        for (auto s : want) {
+            if (mp1[s]) flag = 1;
+        }
+        if (!flag) answer++;
+    }
+
+
+    return answer;
+}
