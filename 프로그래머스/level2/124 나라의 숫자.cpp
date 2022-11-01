6b09b497130227dd5ad9ccaaa94a569559c8a910
LHS-11 <ghktjq1119@naver.com>
2022-11-01 15:24:30 +0900

    Create 124 나라의 숫자.cpp

---
+++ 프로그래머스/level2/124 나라의 숫자.cpp
@@ -0,0 +1,26 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+string s = "";
+string go(int k) {
+
+    if (k == 0) return "";
+
+    string s = "";
+    if (k % 3 == 0) {
+        return s += go(k / 3 - 1) + "4";
+    }
+    else if (k % 3 == 1) {
+        return s += go(k / 3) + "1"; // 4 
+    }
+    else if (k % 3 == 2) {
+        return s += go(k / 3) + "2";
+    }
+    // go(4) -> s=go(1)+"1" -> s=go(0)+"1" -> 0
+
+}
+string solution(int n) {
+    string answer = "";
+    answer = go(n);
+
+    return answer;
+}
