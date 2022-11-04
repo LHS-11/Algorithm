4e2ed544418b591c7177826c4f3647477ac0b9d5
LHS-11 <ghktjq1119@naver.com>
2022-11-04 13:17:39 +0900

    Create 삼각 달팽이.cpp

---
+++ 프로그래머스/level2/삼각 달팽이.cpp
@@ -0,0 +1,47 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+
+    vector<vector<int>> map(n + 1, vector<int>(n + 1, 0));
+    int mx = n * (n + 1) / 2;
+    int num = 1;
+    int range = n;
+    int x = 0, y = -1;
+
+    while (num <= mx) {
+
+        for (int i = y + 1; i <= y + range; i++) {
+            map[i][x] = num++;
+        }
+        y += range;
+        range--;
+        if (num > mx) break;
+
+        for (int i = x + 1; i <= x + range; i++) {
+            map[y][i] = num++;
+        }
+        x += range;
+        range--;
+        if (num > mx) break;
+
+
+        for (int i = 1; i <= range; i++) {
+            map[y - i][x - i] = num++;
+        }
+        x -= range;
+        y -= range;
+        range--;
+
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            answer.push_back(map[i][j]);
+        }
+    }
+
+    return answer;
+}
+
