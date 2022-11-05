551977b33eb5bf2335dd488f275459486a73d404
LHS-11 <ghktjq1119@naver.com>
2022-11-06 01:56:40 +0900

    Update 메뉴 리뉴얼 ( 2021 KAKAO BLIND RECRUITMENT ).cpp

--- 프로그래머스/level2/메뉴 리뉴얼 ( 2021 KAKAO BLIND RECRUITMENT ).cpp
+++ 프로그래머스/level2/메뉴 리뉴얼 ( 2021 KAKAO BLIND RECRUITMENT ).cpp
@@ -1,23 +1,67 @@
 ﻿#include<bits/stdc++.h>
 using namespace std;
 map<string, int> mp;
+vector<string> v;
+vector<tuple<int, int, string>> ans;
+int vis[11];
+map<int, int> sz;
+void go(int idx, int sz, string s, string s1) {
+
+    if (s1.size() == sz) {
+        mp[s1]++;
+        v.push_back(s1);
+        return;
+    }
+
+
+    for (int i = idx; i < s.size(); i++) {
+        if (vis[i]) continue;
+        vis[i] = 1;
+        s1 += s[i];
+        go(i, sz, s, s1);
+        s1.pop_back();
+        vis[i] = 0;
+    }
+}
 vector<string> solution(vector<string> orders, vector<int> course) {
     vector<string> answer;
 
-    for (int i = 0; i < orders.size(); i++) {
-        for (int j = i + 1; j < orders.size(); j++) {
-            for (auto c : orders[i]) {
-                for (auto c1 : orders[j]) {
-                    if (c == c1)
-                }
-            }
+    for (auto str : orders) {
+        for (int i = 2; i <= str.size(); i++) {
+            sort(str.begin(), str.end());
+            go(0, i, str, "");
+            memset(vis, 0, sizeof(vis));
         }
     }
 
-    for (auto str : orders) {
-        cout << str << ' ' << mp[str] << endl;
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+
+    for (auto c : v) {
+        if (mp[c] >= 2) ans.push_back({ c.size(),mp[c],c });
     }
 
+    int chk[11];
+    for (auto n : course) chk[n]++;
 
+    sort(ans.begin(), ans.end());
+
+    for (auto k : ans) {
+        int a, b;
+        string c;
+        tie(a, b, c) = k;
+        if (sz[a] < b) sz[a] = b;
+    }
+
+    for (auto k : ans) {
+        int a, b;
+        string c;
+        tie(a, b, c) = k;
+        if (sz[a] == b && chk[a]) {
+            cout << a << ' ' << b << ' ' << c << endl;
+            answer.push_back(c);
+        }
+    }
+    sort(answer.begin(), answer.end());
     return answer;
 }
