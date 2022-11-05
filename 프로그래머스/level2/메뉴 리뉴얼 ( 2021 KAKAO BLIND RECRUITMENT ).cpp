b41114b25a3c592f9146ce11577c6bc92a957737
LHS-11 <ghktjq1119@naver.com>
2022-11-05 23:58:00 +0900

    Create 메뉴 리뉴얼 ( 2021 KAKAO BLIND RECRUITMENT ).cpp

---
+++ 프로그래머스/level2/메뉴 리뉴얼 ( 2021 KAKAO BLIND RECRUITMENT ).cpp
@@ -0,0 +1,23 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+map<string, int> mp;
+vector<string> solution(vector<string> orders, vector<int> course) {
+    vector<string> answer;
+
+    for (int i = 0; i < orders.size(); i++) {
+        for (int j = i + 1; j < orders.size(); j++) {
+            for (auto c : orders[i]) {
+                for (auto c1 : orders[j]) {
+                    if (c == c1)
+                }
+            }
+        }
+    }
+
+    for (auto str : orders) {
+        cout << str << ' ' << mp[str] << endl;
+    }
+
+
+    return answer;
+}
