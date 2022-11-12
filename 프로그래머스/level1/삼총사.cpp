32031ec3d6f0b6e76f26fd46a4f5e3bf291d4a2b
LHS-11 <ghktjq1119@naver.com>
2022-11-12 22:01:01 +0900

    Create 삼총사.cpp

---
+++ 프로그래머스/level1/삼총사.cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int vis[14], answer;
+void go(vector<int> number, int idx, int k, int sum) {
+
+    if (k == 3) {
+        if (sum == 0) answer++;
+        return;
+    }
+
+    for (int i = idx; i < number.size(); i++) {
+        if (vis[i]) continue;
+        vis[i] = 1;
+        go(number, i, k + 1, sum + number[i]);
+        vis[i] = 0;
+    }
+}
+int solution(vector<int> number) {
+    go(number, 0, 0, 0);
+    return answer;
+}
