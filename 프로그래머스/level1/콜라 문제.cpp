d3b8b136ad4e5042feed5f3fbaca07cd84fb7e50
LHS-11 <ghktjq1119@naver.com>
2022-11-13 11:59:07 +0900

    Create 콜라 문제.cpp

---
+++ 프로그래머스/level1/콜라 문제.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+
+    while (n > a - 1) {
+        int tmp = n / a * b;
+        answer += tmp;
+        tmp += (n % a);
+        n = tmp;
+    }
+    return answer;
+}
