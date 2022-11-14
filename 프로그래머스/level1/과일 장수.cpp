99276f39d7357626fd52c25e433fa21ba758c25e
LHS-11 <ghktjq1119@naver.com>
2022-11-14 13:13:30 +0900

    Create 과일 장수.cpp

---
+++ 프로그래머스/level1/과일 장수.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+#include<algorithm>
+using namespace std;
+int cnt[10];
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+
+    sort(score.begin(), score.end());
+    reverse(score.begin(), score.end());
+
+    int idx = 1;
+    for (int i = 0; i < score.size(); i++) {
+        if (m * idx - 1 == i) {
+            answer += m * score[i];
+            idx++;
+        }
+    }
+
+    return answer;
+}
