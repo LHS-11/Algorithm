f3de52c7d6e2c05737e4e3fed3fbf9394ccb66d7
LHS-11 <ghktjq1119@naver.com>
2022-11-17 01:25:07 +0900

    Create 푸드 파이트 대회.cpp

---
+++ 프로그래머스/level1/푸드 파이트 대회.cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    answer += '0';
+
+    for (int i = food.size() - 1; i > 0; i--) {
+        int n = food[i] / 2;
+
+        while (n) {
+            answer = to_string(i) + answer;
+            answer = answer + to_string(i);
+            n--;
+        }
+    }
+
+    return answer;
+}
