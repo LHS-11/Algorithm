718f43b85a5d87c35ab87c8201bee34ecc2ffe3a
LHS-11 <ghktjq1119@naver.com>
2022-11-19 19:11:25 +0900

    Create 기사단원의 무기.cpp

---
+++ 프로그래머스/level1/기사단원의 무기.cpp
@@ -0,0 +1,27 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int go(int k) {
+    int cnt = 0;
+
+    for (int i = 1; i * i <= k; i++) {
+        if (k % i != 0) continue;
+        if (i * i == k) {
+            cnt++;
+        }
+        else cnt += 2;
+    }
+    return cnt;
+}
+int solution(int number, int limit, int power) {
+    int answer = 0;
+
+    for (int i = 1; i <= number; i++) {
+        int n = go(i);
+        if (n > limit) {
+            answer += power;
+        }
+        else answer += n;
+    }
+
+    return answer;
+}
