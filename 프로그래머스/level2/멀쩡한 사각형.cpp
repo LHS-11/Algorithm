5b9a8e1fb7f4e9ea2f577c8e5a4cf7823cb15964
LHS-11 <ghktjq1119@naver.com>
2022-11-24 16:37:21 +0900

    Create 멀쩡한 사각형.cpp

---
+++ 프로그래머스/level2/멀쩡한 사각형.cpp
@@ -0,0 +1,9 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b) {
+    if (a == 0) return b;
+    return gcd(b % a, a);
+}
+long long solution(int w, int h) {
+    return ((long long)w * h) - (w + h) + gcd(w, h);
+}
