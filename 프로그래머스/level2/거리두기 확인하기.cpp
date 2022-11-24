2a7e14d0a1c29fbb7a31b0cf7e3b0f0a1e4dd4df
LHS-11 <ghktjq1119@naver.com>
2022-11-24 13:56:39 +0900

    Update 거리두기 확인하기.cpp

--- 프로그래머스/level2/거리두기 확인하기.cpp
+++ 프로그래머스/level2/거리두기 확인하기.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 int dx[4] = { -1,0,1,0 };
 int dy[4] = { 0,-1,0,1 };
-int dx1[8] = { -2,0,2,0,-1,-1,1,1 };
-int dy1[8] = { 0,-2,0,2,-1,1,-1,1 };
+int vis[5][5];
 vector<int> solution(vector<vector<string>> places) {
     vector<int> answer;
 
@@ -14,59 +13,40 @@ vector<int> solution(vector<vector<string>> places) {
         int flag = 0;
         for (int i = 0; i < s.size(); i++) {
             for (int j = 0; j < s[i].size(); j++) {
-
-                queue<pair<int, int>> q;
-
                 if (s[i][j] == 'P') {
-                    q.push({ i,j });
-                }
+                    flag = 0;
+                    queue<tuple<int, int, int>> q;
+                    q.push({ i,j,0 });
+                    vis[i][j] = 1;
+                    while (!q.empty()) {
+                        int x, y, cnt;
+                        tie(y, x, cnt) = q.front();
 
-                while (!q.empty()) {
+                        q.pop();
 
-                    auto cur = q.front();
-                    q.pop();
-                    for (int i = 0; i < 4; i++) {
-                        int nx = cur.second + dx[i];
-                        int ny = cur.first + dy[i];
-
-                        if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) continue;
-                        if (s[ny][nx] == 'P') {
+                        if (s[y][x] == 'P' && !(i == y && j == x)) {
                             flag = 1;
                             break;
                         }
 
-                    }
-                    if (flag) break;
-                    else {
-                        queue<tuple<int, int, int>> q1;
-                        q1.push({ cur.first,cur.second,0 });
+                        for (int i = 0; i < 4; i++) {
+                            int nx = x + dx[i];
+                            int ny = y + dy[i];
 
-                        while (q1.empty()) {
-                            int x, y, cnt;
-                            tie(y, x, cnt) = q1.front();
-                            q1.pop();
+                            if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) continue;
 
-                            if (s[y][x] == 'P') {
-                                flag = 1;
-                                break;
-                            }
-                            for (int i = 0; i < 8; i++) {
-                                int nx = cur.second + dx1[i];
-                                int ny = cur.first + dy1[i];
-                                if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) continue;
-                                if (s[ny][nx] == 'O' && cnt < 2) {
-                                    q1.push({ ny,nx,cnt + 1 });
-                                }
-
+                            if (s[ny][nx] != 'X' && cnt < 2) {
+                                q.push({ ny,nx,cnt + 1 });
                             }
                         }
                     }
+                    if (flag) break;
                 }
                 if (flag) break;
             }
             if (flag) break;
-        }
 
+        }
         if (!flag) answer.push_back(1);
         else answer.push_back(0);
 
