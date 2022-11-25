7a32e69e53f8b3a5470adb1d5a20da33c25e83e5
LHS-11 <ghktjq1119@naver.com>
2022-11-25 15:27:05 +0900

    Create 문자열 압축 ( 2020 KAKAO BLIND RECRUITMENT ).cpp

---
+++ 프로그래머스/level2/문자열 압축 ( 2020 KAKAO BLIND RECRUITMENT ).cpp
@@ -0,0 +1,34 @@
+﻿#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+int solution(string s) {
+    int answer = INT_MAX;
+
+    for (int i = 1; i <= s.size(); i++) {
+
+        string ans = "";
+        int cnt = 1;
+
+        string s1 = s.substr(0, i);
+        for (int j = i; j < s.size(); j += i) {
+            string s2 = s.substr(j, i);
+            if (s1 == s2) {
+                cnt++;
+            }
+            else {
+                if (cnt >= 2) ans += to_string(cnt) + s1;
+                else ans += s1;
+                s1 = s.substr(j, i);
+                cnt = 1;
+            }
+        }
+        if (cnt >= 2) ans += to_string(cnt) + s1;
+        else ans += s1;
+
+        int sz = ans.size();
+        answer = min(answer, sz);
+
+    }
+    return answer;
+
+}
