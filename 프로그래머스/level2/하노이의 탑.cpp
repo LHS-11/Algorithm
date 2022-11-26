cb1eb4f61421cca800097bdede559caf4850bd42
LHS-11 <ghktjq1119@naver.com>
2022-11-26 14:18:43 +0900

    Create 하노이의 탑.cpp

---
+++ 프로그래머스/level2/하노이의 탑.cpp
@@ -0,0 +1,22 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+void go(vector<vector<int>>& v, int k, int st, int en) {
+
+    if (k == 1) {
+        v.push_back({ st,en });
+        return;
+    }
+
+    go(v, k - 1, st, 6 - st - en);
+    v.push_back({ st,en });
+    go(v, k - 1, 6 - st - en, en);
+
+
+}
+vector<vector<int>> solution(int n) {
+    vector<vector<int>> answer;
+
+    go(answer, n, 1, 3);
+
+    return answer;
+}
