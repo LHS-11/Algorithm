928cc56d5f6bc55d3742b47d9cb2a5d8a03fa0ce
LHS-11 <ghktjq1119@naver.com>
2022-11-27 11:25:48 +0900

    Create 귤 고르기.cpp

---
+++ 프로그래머스/level2/귤 고르기.cpp
@@ -0,0 +1,35 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+vector<pair<int, int>> v;
+int solution(int k, vector<int> tangerine) {
+    int answer = 0;
+    // 1 2 2 3 3 4 5 5
+    vector<int> t = tangerine;
+    sort(t.begin(), t.end());
+
+
+    map<int, int> mp;
+    for (auto e : t) {
+        mp[e]++;
+    }
+
+    for (auto e : t) {
+        v.push_back({ mp[e],e });
+    }
+
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+
+    for (int i = v.size() - 1; i >= 0; i--) {
+        int cnt = v[i].first;
+
+        if (k <= 0) break;
+        else {
+            k -= cnt;
+            answer++;
+        }
+    }
+
+
+    return answer;
+}
