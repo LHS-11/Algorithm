980a243ec551f6ed1f531f5f63b5578f254cf307
LHS-11 <ghktjq1119@naver.com>
2022-11-28 13:07:38 +0900

    Create 택배상자.cpp

---
+++ 프로그래머스/level2/택배상자.cpp
@@ -0,0 +1,46 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int solution(vector<int> order) {
+    int answer = 0;
+
+    stack<int> stk;
+    int st = 1, i = 0;
+
+    while (1) {
+        if (st == order[i]) {
+            i++;
+            st++;
+            answer++;
+        }
+        if (st > order[i]) {
+            int flag = 0;
+            while (stk.size() && stk.top() == order[i]) {
+                stk.pop();
+                answer++;
+                i++;
+                flag = 1;
+            }
+            if (!flag) break;
+        }
+        else if (st < order[i]) {
+            stk.push(st);
+            st++;
+        }
+        if (st > order.size()) break;
+    }
+
+    //stack<int> stk;
+
+    //int i = 0;
+    //for (int st = 1; st <= order.size(); st++) {
+    //    stk.push(st);
+
+    //    while (stk.size() && stk.top() == order[i]) {
+    //        stk.pop();
+    //        i++;
+    //        answer++;
+    //    }
+    //}
+
+    return answer;
+}
