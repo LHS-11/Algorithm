8a3512960aac081d18a8b67280a05595cc938e10
LHS-11 <ghktjq1119@naver.com>
2022-11-29 14:47:47 +0900

    Create N-Queen.cpp

---
+++ 프로그래머스/level2/N-Queen.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+int vis[13], cnt; // 행 : i, 열 : vis[i]
+bool check(int i) {
+    for (int j = 0; j < i; j++) {
+        if (vis[i] == vis[j] || (abs(vis[i] - vis[j]) == i - j)) return false;
+    }
+    return true;
+}
+void go(int h, int n) {
+
+    if (h == n) {
+        cnt++;
+        return;
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        vis[h] = i;
+        if (check(h)) {
+            go(h + 1, n);
+        }
+    }
+
+}
+int solution(int n) {
+    int answer = 0;
+    go(0, n);
+    answer = cnt;
+    return answer;
+}
