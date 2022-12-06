54437cf779ab0c0fc932b726470d108f6b3b0e4a
LHS-11 <ghktjq1119@naver.com>
2022-12-07 00:43:48 +0900

    Create 점 찍기.cpp

---
+++ 프로그래머스/level2/점 찍기.cpp
@@ -0,0 +1,12 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+long long solution(int k, int d) {
+    long long answer = 0;
+
+    for (int i = 0; i <= d; i += k) {
+        long long y = sqrt((long long)d * d - (long long)i * i);
+        answer += (y / k) + 1;
+    }
+
+    return answer;
+}
