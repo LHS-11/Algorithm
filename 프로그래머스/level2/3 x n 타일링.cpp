f04c16e7ae26f9e4c47ddd2aade38204b80d7de9
LHS-11 <ghktjq1119@naver.com>
2022-12-08 19:22:32 +0900

    Create 3 x n 타일링.cpp

---
+++ 프로그래머스/level2/3 x n 타일링.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+#define mod 1000000007
+long long dp[5004];
+int solution(int n) {
+
+    dp[0] = 1;
+    dp[1] = 0, dp[3] = 0, dp[5] = 0;
+    dp[2] = 3;
+    dp[4] = 11;
+    for (int i = 6; i <= n; i++) {
+        if (i % 2 == 0) {
+            dp[i] += (dp[i - 2] * 3) % mod; // dp[4]=dp[2]*3
+            for (int j = 2; j <= i - 4; j += 2) {
+                dp[i] += (2 * dp[j]) % mod;
+            }
+            dp[i] += 2;
+        }
+        else dp[i] = 0;
+        dp[i] %= mod;
+    }
+
+    return dp[n];
+}
