de0739b1caec2a19b2be7fb25154f9f97ee93ee8
LHS-11 <ghktjq1119@naver.com>
2022-12-09 16:26:29 +0900

    Create 디펜스 게임.cpp

---
+++ 프로그래머스/level2/디펜스 게임.cpp
@@ -0,0 +1,30 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+// 무적권을 최대한 많은 적수가 나왔을 때 써야 최적화이지 않을까?
+// 무적권을 쓸지 안쓸지를 선택해야함 -> 어떻게 해야할까?
+// 만약 현재 가지고 있는 병사보다 적의 수가 크다면 무적권을 무조건 사용해야함
+// 
+int solution(int n, int k, vector<int> enemy) {
+    int answer = enemy.size();
+
+    priority_queue<int> pq;
+
+    for (int i = 0; i < enemy.size(); i++) {
+        pq.push(enemy[i]);
+        n -= enemy[i];
+        if (n < 0) {
+            if (k > 0 && pq.size()) {
+                k--;
+                n += pq.top();
+                pq.pop();
+            }
+            else {
+                answer = i;
+                break;
+            }
+        }
+    }
+
+
+    return answer;
+}
