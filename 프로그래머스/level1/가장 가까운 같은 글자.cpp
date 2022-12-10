348e78a0563913c7fa5d4f8cdd1a5c63fc5df785
LHS-11 <ghktjq1119@naver.com>
2022-12-10 22:00:00 +0900

    Create 가장 가까운 같은 글자.cpp

---
+++ 프로그래머스/level1/가장 가까운 같은 글자.cpp
@@ -0,0 +1,22 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+// 위치표시 
+int vis[10004];
+vector<int> solution(string s) {
+    vector<int> answer;
+
+    fill(vis, vis + 10004, -1);
+
+    for (int i = 0; i < s.size(); i++) {
+        if (vis[s[i] - 'a'] == -1) {
+            answer.push_back(-1);
+        }
+        else {
+            answer.push_back(i - vis[s[i] - 'a']);
+        }
+        vis[s[i] - 'a'] = i;
+    }
+
+
+    return answer;
+}
