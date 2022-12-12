8cc781f3a1260ea1135b8e4f8ec72a04d844ead1
LHS-11 <ghktjq1119@naver.com>
2022-12-12 15:23:37 +0900

    Create 명예의 전당 (1).cpp

---
+++ 프로그래머스/level1/명예의 전당 (1).cpp
@@ -0,0 +1,15 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+priority_queue<int, vector<int>, greater<int>> q;
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+
+    for (auto s : score) {
+        q.push(s);
+        if (q.size() > k) q.pop();
+        answer.push_back(q.top());
+    }
+
+
+    return answer;
+}
