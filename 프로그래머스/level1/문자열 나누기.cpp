7e902b5c7fdd65eb29d263ddf39bf45726f2af46
LHS-11 <ghktjq1119@naver.com>
2022-12-14 11:45:47 +0900

    Create 문자열 나누기.cpp

---
+++ 프로그래머스/level1/문자열 나누기.cpp
@@ -0,0 +1,29 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+// 카운트 변수 두개를 둬서 계속해서 비교 하는식
+int cnt1, cnt2, flag;
+int solution(string s) {
+    int answer = 0;
+
+    char x = s[0];
+    cnt1++;
+    for (int i = 1; i < s.size(); i++) {
+        if (x == s[i]) {
+            cnt1++;
+        }
+        else {
+            cnt2++;
+        }
+        if (cnt1 == cnt2) {
+            answer++;
+            x = s[i + 1];
+            i++;
+            cnt1 = 1;
+            cnt2 = 0;
+
+        }
+    }
+    if (x) answer++;
+
+    return answer;
+}
