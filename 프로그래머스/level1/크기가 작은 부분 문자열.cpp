f6d0d74eaf0716dc0689300bbb24a267ab45d26d
LHS-11 <ghktjq1119@naver.com>
2022-12-22 21:30:37 +0900

    Create 크기가 작은 부분 문자열.cpp

---
+++ 프로그래머스/level1/크기가 작은 부분 문자열.cpp
@@ -0,0 +1,13 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int solution(string t, string p) {
+    int answer = 0;
+    int sz = p.size();
+
+    for (int i = 0; i <= t.size() - sz; i++) {
+        string s = t.substr(i, sz);
+        if (p >= s) answer++;
+    }
+
+    return answer;
+}
