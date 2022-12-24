f123f0c9335f56144bcb783904422c04a7e181dd
LHS-11 <ghktjq1119@naver.com>
2022-12-24 23:05:26 +0900

    Create 단어 변환.cpp

---
+++ 프로그래머스/level3/단어 변환.cpp
@@ -0,0 +1,40 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int vis[54];
+bool check(string a, string b) {
+    int sz = a.size();
+    int cnt = 0;
+    for (int i = 0; i < sz; i++) {
+        if (a[i] != b[i]) {
+            cnt++;
+        }
+    }
+    if (cnt >= 2) {
+        return false;
+    }
+    else return true;
+}
+int solution(string begin, string target, vector<string> words) {
+    int answer = 0;
+
+
+    queue<pair<string, int>> q;
+    q.push({ begin,0 });
+
+    while (q.size()) {
+        auto cur = q.front();
+        q.pop();
+
+        if (cur.first == target) {
+            return cur.second;
+        }
+
+        for (int i = 0; i < words.size(); i++) {
+            if (check(cur.first, words[i]) && !vis[i]) {
+                vis[i] = 1;
+                q.push({ words[i],cur.second + 1 });
+            }
+        }
+    }
+    return answer;
+}
