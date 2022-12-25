b4e3d2aa581a0d2e37ae86c7b7f4db0667424bc8
LHS-11 <ghktjq1119@naver.com>
2022-12-25 11:49:23 +0900

    Create 야근 지수.cpp

---
+++ 프로그래머스/level3/야근 지수.cpp
@@ -0,0 +1,26 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+long long solution(int n, vector<int> works) {
+    long long answer = 0;
+    // 큰 수를 계속 줄여야 함 
+    sort(works.begin(), works.end());
+
+    int mx = works[works.size() - 1];
+
+    while (n--) {
+        int idx = lower_bound(works.begin(), works.end(), mx) - works.begin();
+        works[idx]--;
+        if (idx == works.size() - 1) {
+            mx = works[idx];
+        }
+    }
+
+    for (auto w : works) {
+        if (w <= 0) continue;
+        answer += (w * w);
+    }
+
+
+    return answer;
+}
