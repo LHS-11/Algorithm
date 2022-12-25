124ddaa0be6e44eabbb638e6ca6f5d4074f20707
LHS-11 <ghktjq1119@naver.com>
2022-12-25 11:49:13 +0900

    Create 등굣길.cpp

---
+++ 프로그래머스/level3/등굣길.cpp
@@ -0,0 +1,24 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+int dp[104][104];
+int p[104][104];
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
+
+    dp[0][0] = 1;
+
+    for (auto puddle : puddles) {
+        p[puddle[1] - 1][puddle[0] - 1] = 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (p[i][j]) continue;
+            if (i - 1 >= 0) dp[i][j] += dp[i - 1][j];
+            if (j - 1 >= 0) dp[i][j] += dp[i][j - 1];
+            dp[i][j] %= mod;
+        }
+    }
+    return dp[n - 1][m - 1];
+}
