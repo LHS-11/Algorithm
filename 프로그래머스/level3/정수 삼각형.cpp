b9c14f295ffca7beb7b0ca235c815427d1f9d2f7
LHS-11 <ghktjq1119@naver.com>
2022-12-26 14:01:04 +0900

    Create 정수 삼각형.cpp

---
+++ 프로그래머스/level3/정수 삼각형.cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int dp[504][10000];// dp[층수][몇번째]
+int solution(vector<vector<int>> triangle) {
+
+    int sz = triangle.size() - 1;
+
+    for (int i = 0; i < triangle[sz].size(); i++) {
+        dp[sz][i] = triangle[sz][i];
+    }
+
+    for (int i = triangle.size() - 2; i >= 0; i--) { // 3~ 0
+        for (int j = 0; j < triangle[i].size(); j++) { // 4
+            dp[i][j] = max(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+        }
+    }
+
+    return dp[0][0];
+}
