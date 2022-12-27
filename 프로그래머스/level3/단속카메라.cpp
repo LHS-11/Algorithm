fd59a0754d3ac8fd23d86c86715684cce99f69b9
LHS-11 <ghktjq1119@naver.com>
2022-12-28 04:14:28 +0900

    Create 단속카메라.cpp

---
+++ 프로그래머스/level3/단속카메라.cpp
@@ -0,0 +1,24 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+bool cmp(vector<int> r1, vector<int> r2) {
+    if (r1[1] == r2[1]) {
+        return r1[0] < r2[0];
+    }
+    return r1[1] < r2[1];
+}
+int solution(vector<vector<int>> routes) {
+    int answer = 0;
+
+    sort(routes.begin(), routes.end(), cmp);
+
+    int st = routes[0][1];
+    answer++;
+    for (int i = 1; i < routes.size(); i++) {
+        if (st < routes[i][0]) {
+            st = routes[i][1];
+            answer++;
+        }
+    }
+
+    return answer;
+}
