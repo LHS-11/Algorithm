8dfd2c28f53351d09b32038023f2b8f17d39aefc
HwaSup <ghktjq1119@naver.com>
2023-02-20 18:20:16 +0900

    Create 보행자 천국.cpp

---
+++ 프로그래머스/level3/보행자 천국.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int MOD = 20170805;
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+int solution(int m, int n, vector<vector<int>> city_map) {
+    int dp[504][504][2]={0};
+    // fill 로 초기화를 시켰는데 왜 대체... 
+    fill(&dp[0][0][0],&dp[0][0][0]+n*m*2,0);
+    
+    dp[0][0][0]=1;
+    dp[0][0][1]=1;
+    // 위쪽에서 내려오는 dp 초기화
+    for(int i=1;i<m;i++) {
+        if(city_map[i][0]==1) break;
+        dp[i][0][0]=1;
+    }
+    
+    // 아래쪽에서 내려오는 dp 초기화
+    for(int i=1;i<n;i++){
+        if(city_map[0][i]==1) break;
+        dp[0][i][1]=1;
+    }
+    
+    
+    for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+
+            // 위쪽에서 온 경우
+            if(city_map[i-1][j]!=1){
+                dp[i][j][0]+=dp[i-1][j][0];
+                if(city_map[i-1][j]!=2) dp[i][j][0]+=dp[i-1][j][1];
+            }
+            // 왼쪽에서 온 경우
+            if(city_map[i][j-1]!=1){
+                dp[i][j][1]+=dp[i][j-1][1];
+                if(city_map[i][j-1]!=2) dp[i][j][1]+=dp[i][j-1][0];
+            }
+            dp[i][j][0]%=MOD;
+            dp[i][j][1]%=MOD;
+            
+        }
+    }
+    
+    return (dp[m-1][n-1][0]+dp[m-1][n-1][1])%MOD;
+}
