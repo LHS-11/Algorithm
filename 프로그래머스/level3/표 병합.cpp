0f412bd9cb33ab279b280b083298f97a8a02fa20
HwaSup <ghktjq1119@naver.com>
2023-03-18 19:00:51 +0900

    Create 표 병합.cpp

---
+++ 프로그래머스/level3/표 병합.cpp
@@ -0,0 +1,216 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+#include<bits/stdc++.h>
+using namespace std;
+string board[54][54];
+// merge 된거 구분
+int check[54][54];
+int check1[54][54];
+const int sz=51;
+vector<string> solution(vector<string> commands) {
+    vector<string> answer;
+    int num=1;
+    for(int i=0;i<54;i++){
+        for(int j=0;j<54;j++){
+            board[i][j]=".";
+            check[i][j]=num;
+            check1[i][j]=num;
+            num++;
+        }
+    }
+    for(auto c : commands){
+        vector<string> cmd;
+        string str="";
+        for(int i=0;i<c.size();i++){
+            if(c[i]==' '){
+                cmd.push_back(str);
+                str="";
+            }else str+=c[i];
+        }
+        cmd.push_back(str);
+
+        if(cmd[0]=="UPDATE"){
+            if(cmd.size()==4){
+                int r=stoi(cmd[1]);
+                int c=stoi(cmd[2]);
+                string val=cmd[3];
+
+                int chk=check[r][c];
+                for(int i=0;i<sz;i++){
+                    for(int j=0;j<sz;j++){
+                        if(check[i][j]==chk){
+                            board[i][j]=val;
+                        }
+                    }
+                }
+
+            }else{
+                string val1=cmd[1];
+                string val2=cmd[2];
+                for(int i=0;i<sz;i++){
+                    for(int j=0;j<sz;j++){
+                        if(board[i][j]==val1){
+                            board[i][j]=val2;
+                        }
+                    }
+                }
+            }
+        }
+        else if(cmd[0]=="MERGE"){
+            int r1=stoi(cmd[1]);
+            int c1=stoi(cmd[2]);
+            int r2=stoi(cmd[3]);
+            int c2=stoi(cmd[4]);
+            int chk1=check[r1][c1];
+            int chk2=check[r2][c2];
+            string bd=board[r1][c1];
+
+            if(board[r1][c1]=="." && board[r2][c2]!="."){
+                int tmp=chk1;
+                chk1=chk2;
+                chk2=tmp;
+                bd=board[r2][c2];
+            }
+
+            for(int i=1;i<sz;i++){
+                for(int j=1;j<sz;j++){
+                    if(chk1==check[i][j] || chk2==check[i][j]){
+                        check[i][j]=chk1;
+                        board[i][j]=bd;
+                    }
+                }
+            }
+
+        }else if(cmd[0]=="UNMERGE"){
+            int r=stoi(cmd[1]);
+            int c=stoi(cmd[2]);
+            int chk=check[r][c];
+            string val=board[r][c];
+            if(!chk) continue;
+            for(int i=0;i<sz;i++){
+                for(int j=0;j<sz;j++){
+                    if(chk==check[i][j]){
+                        check[i][j]=check1[i][j];
+                        board[i][j]=".";
+                    }
+                }
+            }
+            check[r][c]=check1[r][c];
+            board[r][c]=val;
+        }else{
+            int r=stoi(cmd[1]);
+            int c=stoi(cmd[2]);
+            if(board[r][c]==".") answer.push_back("EMPTY");
+            else answer.push_back(board[r][c]);
+        }
+    }
+
+    return answer;
+}
