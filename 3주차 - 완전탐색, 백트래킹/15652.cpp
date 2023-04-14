129572e875231fb79c1da67f000bf8f3db199ae1
HwaSup <ghktjq1119@naver.com>
2023-04-15 00:09:31 +0900

    Create 15652.cpp

---
+++ 3주차 - 완전탐색, 백트래킹/15652.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,vis[9];
+vector<int> v;
+void go(int idx){
+	if(v.size()==m){
+		for(auto x :v) cout<<x<<' ';
+		cout<<'\n';
+		return;
+	}
+	
+	for(int i=idx ;i<=n;i++){
+		v.push_back(i);
+		go(i);
+		v.pop_back();
+	}
+}
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);
+	
+	cin>> n >> m;
+	go(1);
+} 
