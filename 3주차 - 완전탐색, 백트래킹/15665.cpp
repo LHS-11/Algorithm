737f6c351d1f30f68c9a090acb919d8be583c2f9
HwaSup <ghktjq1119@naver.com>
2023-04-18 01:01:33 +0900

    Create 15665.cpp

---
+++ 3주차 - 완전탐색, 백트래킹/15665.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,vis[9];
+int chk;
+vector<int> v;
+vector<int> arr;
+void go(int idx){
+	if(v.size()==m){
+		for(auto x :v) cout<<x<<' ';
+		cout<<'\n';
+		return;
+	}
+	
+	chk=-1;
+	for(int i=idx;i<n;i++){
+
+		if(vis[i] || chk==arr[i]) continue;
+		v.push_back(arr[i]);
+		go(i);
+		chk=arr[i];
+		v.pop_back();
+
+	}
+}
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);
+	
+	cin>> n >> m;
+	for(int i=0;i<n;i++) {
+		int x;
+		cin >> x;
+		arr.push_back(x);
+	}
+	sort(arr.begin(),arr.end());
+	
+	go(0);
+} 
