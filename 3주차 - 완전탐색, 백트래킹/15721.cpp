3bd49930283718e6592412a1cb33ab205397fdf9
HwaSup <ghktjq1119@naver.com>
2023-06-09 23:18:06 +0900

    Create 15721.cpp

---
+++ 3주차 - 완전탐색, 백트래킹/15721.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a,t,c;
+int arr[]={0,1,0,1};
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);
+	
+	cin >> a >> t>>c; 
+	
+	int ans=0;
+	int idx=2;
+	while(t){
+		int flag=0;
+		for(int i=0;i<4;i++){
+			if(arr[i]==1) cout<<"单扁"<<endl;
+			else cout<<"环"<<endl;
+			if(arr[i]==c){
+				t--;
+			}
+			if(t==0){
+				flag=1;
+				break;
+			}
+			ans++;
+		}
+		
+		if(flag){
+			break;
+		}
+		
+		for(int i=0;i<idx;i++){
+			cout<<"环"<<endl; 
+			if(0==c){
+				t--;
+			}
+			if(t==0){
+				flag=1;
+				break;
+			}
+			ans++;
+		}
+		
+		if(flag){
+			break;
+		}
+		
+		for(int i=0;i<idx;i++){
+			cout<<"单扁"<<endl;
+			if(1==c){
+				t--;
+			}
+			if(t==0){
+				flag=1;
+				break;
+			}
+			ans++;
+		}
+		
+		if(flag){
+			break;
+		}
+		
+		idx++;
+	}
+	cout<<ans%a;
+	
+	
+}
